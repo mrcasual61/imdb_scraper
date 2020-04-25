9d8faec07a6cb6616193ab80da39b09109f29506
mrcasual61 <44846765+mrcasual61@users.noreply.github.com>
2020-04-25 12:06:48 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sci-fi Scraper</title>
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>          
+<script type="text/javascript">
+    // Check if the page has loaded completely
+    /*                                       
+    $(document).ready( function() { 
+        $('#php_output').load('scifi_scraper.php', function(response, status, http) { 
+            if(status == "success") 
+                $("#spinner").hide();
+            	$("#php_output").show();
+            if(status == "error") 
+                alert("Error: " + http.status + ": " + http.statusText); 
+        });
+    });
+*/
+    $(document).ready( function() { 
+	    $.ajax({
+	        type: "GET",
+	        url: "scifi_scraper.php",
+	        dataType: "json",
+	        async: false,
+	        success: function(JSONObject) {
+	        	//var len = data.movies.length;
+	        	var content = "";
+	        	for (var key in JSONObject) {
+	        		if (JSONObject.hasOwnProperty(key)) {
+	        			content += "<div class='movie'>";
+            			content += "<div class='image'><img src='" + JSONObject[key]["image_src"] + "'></div>";
+	        			content += "<div class='info'>";
+            			content += "<div class='title'><h3>" + JSONObject[key]["title"] + "</h3></div>";
+	        			content += "<div class='director_stars'>";
+            			content += "<div class='director'><strong>Director: </strong>" + JSONObject[key]["director"] + "</div>";
+            			content += "<div class='stars'><strong>Stars: </strong>" + JSONObject[key]["stars"] + "</div>";
+            			content += "<div class='stars'><strong>Release Date: </strong>" + JSONObject[key]["release_date"] + "</div>";
+            			content += "<div class='stars'><strong>IMDB Rating: </strong>" + JSONObject[key]["rating"] + "</div>";
+          				content += "</div>";
+          				content += "</div>";
+            			content += "<div class='storyline'>" + JSONObject[key]["storyline"] + "</div>";
+          				content += "</div>";
+          			}
+          		}
+
+	            $("#php_output").html(content);
+                $("#spinner").hide();
+            	$("#php_output").show();
+     		},
+     		error: function(xhr, status, error){
+         		var errorMessage = xhr.status + ': ' + xhr.statusText;
+         		alert('Error - ' + errorMessage);
+	        }
+        });
+    });
+</script> 
+
+<style>
+
+	body {
+		font-family:  Arial;
+	}
+
+	h3 {
+		margin: 0px 0px 8px 0px;
+	}
+
+	#main {
+		margin: 20px auto;
+		max-width: 1000px;
+		text-align: center;
+	}
+
+	#spinner {
+		padding-top: 15%;
+		text-align: center;
+	}
+
+	#php_output {
+		display: none;
+	}
+
+	.movie {
+		border: 1px solid #ccc;
+    	display: inline-block;
+		margin: auto;
+		margin-bottom: 20px;
+		text-align: left;
+		width: 600px;
+	}
+
+	.image {
+		float: left;
+		height: 120px;
+		padding: 15px;
+		width: 15%;
+	}
+
+	.info {
+		height: 120px;
+		padding: 15px;
+	}
+
+	.director_stars {
+		font-size: 14px;
+		font-style: italic;
+	}
+
+	.storyline {
+		border-top: 1px solid #ccc;
+		padding: 15px;
+		text-align: justify;
+	}
+
+
+</style>
+
+</head>
+
+<body>
+
+	<div id="spinner">
+		<image src="images/spinner.gif" width="250" height="250">
+	</div>
+
+	<div id="main">
+		<div id="php_output"></div>
+	</div>
+
+</body>
+
+</html>
---
+++ scifi_scraper.php
@@ -0,0 +1,237 @@
+<?php
+require_once "simple_html_dom.php";
+
+set_time_limit(60);
+
+//get html content from the site.
+$dom = new DOMDocument();
+$dom->preserveWhiteSpace = false;
+
+// Collect movies into an array.
+$movies = array();
+$i = 0;
+$p = 0;
+
+function get_dom($url, $caller) {
+
+	global $dom;
+	$dom = file_get_html($url, false);
+	// Based on which function calls this function.
+	switch ($caller) {
+		case "get_movies":
+			get_movies($dom);
+			break;
+		case "more_info":
+			return $dom;
+			break;
+		default:
+			break;
+	}
+
+}
+
+
+function get_movies($dom) {
+
+	global $movies, $i, $p;
+
+	if(!empty($dom)) {
+		foreach($dom->find(".lister-item") as $div_class) {
+			foreach($div_class->getElementsByTagName('h3') as $div_header) {
+				// Link
+				$links = $div_header->getElementsByTagName('a');
+				foreach($links as $link) {
+					$movies[$i]['link'] = $link->getAttribute('href');
+				}
+				// Title
+				foreach($links as $key=>$var) {
+					$movies[$i]['title'] = html_entity_decode($var->plaintext);
+				}
+			}
+			// IMDB rating.
+			$imdb_rating = $div_class->find(".ratings-imdb-rating", 0);
+			$movies[$i]['rating'] = trim($imdb_rating->plaintext);
+
+			//  Director, stars.
+			$director = "";
+			$stars = "";
+			// Get all text for the div.
+			$all_text = html_entity_decode($div_class->plaintext);
+			//echo $all_text . "<br />";
+			// Find starting poit for Directors.
+			$director_start = strpos($all_text, "Director") + 10;
+			// Find ending point for Stars.
+			$stars_end = strpos($all_text, "Votes") - 2;
+			//  Find starting point for Stars.
+			// Possibly no Stars.  Check first.
+			$stars_start = strpos($all_text, "Stars:");
+			if ($stars_start === false) {
+				// Use $stars_end value for use with Director find.
+				$stars_start = $stars_end;
+			} else {
+				$stars_start = $stars_start + 6;
+				$stars = trim(substr($all_text, $stars_start, $stars_end - $stars_start));
+			}
+			
+			// Director
+			$director = trim(substr($all_text, $director_start, $stars_start - $director_start - 20));
+
+			$movies[$i]['director'] = $director;
+			$movies[$i]['stars'] = $stars;
+
+			// Get image info.
+			foreach ($div_class->find(".lister-item-image") as $image_section) {
+				// Image src.
+				$image_srcs = $image_section->getElementsByTagName('img');
+				$image_src = $image_srcs[0]->getAttribute('loadlate');
+			}
+			$movies[$i]['image_src'] = $image_src;
+		
+			$i++;
+			if ($i == 2) {
+				break;
+			}
+		}
+
+		// Go to next page.
+		$next_links = $dom->find(".lister-page-next");
+		$next_link = "https://www.imdb.com" . $next_links[0]->getAttribute('href');
+
+		$p++;
+		if ($p < 1) {
+			get_dom($next_link, "get_movies");
+		} else {
+			more_info();
+		}
+	}
+
+}
+
+
+function more_info() {
+
+	global $dom, $movies;
+
+	// Get info from each movie page.
+	$count = count($movies);
+
+	for ($i = 0; $i < $count; $i++) {
+		//echo $i . " - " . $movies[$i]['link'] . "<br />";
+		$link = "https://www.imdb.com" . $movies[$i]['link'];
+		// Get dom.
+		get_dom($link, "more_info");
+		// Get storyline.
+		$storyline_section = $dom->find("#titleStoryLine", 0);
+		$storyline = $storyline_section->getElementsByTagName('p');
+		//echo $storyline[0] . "**<br /><br />";
+		$movies[$i]['storyline'] = trim(html_entity_decode($storyline[0]->plaintext));
+
+		// Get release date.
+		$release_date_section = $dom->find("#titleDetails", 0)->plaintext;
+		//echo $release_date_section . "<br /><br />";
+
+		// Find starting poit for Release Date.
+		$release_start = strpos($release_date_section, "Release Date:");
+		// Find ending point for Release Date.
+		$release_end = strpos($release_date_section, "See more", $release_start);
+
+		if ($release_start === false || $release_end === false) {
+			$movies[$i]['release_date'] = "";
+		} else {
+			$release_start = $release_start + 14;
+			$release_end = $release_end - 2;
+			$release_date = trim(substr($release_date_section, $release_start, $release_end - $release_start));
+			$movies[$i]['release_date'] = $release_date;
+		}
+
+	}
+
+	db_connect();
+
+}
+
+
+function db_connect() {
+
+	$servername = "scifi.db.10929588.d55.hostedresource.net";
+	$username = "scifi";
+	$password = "Aliens@61";
+	$db = "scifi";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $db);
+	mysqli_set_charset($conn, 'utf8');
+
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error . "<br /><br />");
+	} else {
+		db_insert($conn);
+	}
+
+}
+
+
+function db_insert($conn) {
+
+	global $movies;
+
+	if(is_array($movies)){
+
+	    for ($i = 0; $i < sizeof($movies); $i++){
+	        $link = mysqli_real_escape_string($conn, $movies[$i]['link']);
+	        $title = mysqli_real_escape_string($conn, $movies[$i]['title']);
+	        $rating = mysqli_real_escape_string($conn, $movies[$i]['rating']);
+	        $director = mysqli_real_escape_string($conn, $movies[$i]['director']);
+	        $stars = mysqli_real_escape_string($conn, $movies[$i]['stars']);
+	        $image_src = mysqli_real_escape_string($conn, $movies[$i]['image_src']);
+	        $storyline = mysqli_real_escape_string($conn, $movies[$i]['storyline']);
+	        $release_date = mysqli_real_escape_string($conn, $movies[$i]['release_date']);
+
+	        $sql .= "INSERT INTO scifi_movies (link, title, rating, director, stars, image_src, storyline, release_date) values ('$link', '$title', '$rating', '$director', '$stars', '$image_src', '$storyline', '$release_date');";
+	    }
+
+	    $sql = rtrim($sql, ";");
+	    if (!$result = mysqli_multi_query($conn, $sql)) {
+		    echo mysqli_error($conn);
+		}
+
+	}
+
+	create_csv();
+
+}
+
+
+function create_csv() {
+
+	global $movies;
+	$array = array();
+
+	// Open file.
+	$fo = fopen('scifi_movies.csv','w');
+
+	// Loop through array.
+	foreach ($movies as $movie) {
+		foreach ($movie as $index => $value) {
+			$array[$index] = utf8_encode($value);
+		}
+		fputs($fo, $bom =( chr(0xEF) . chr(0xBB) . chr(0xBF) ));
+		fputcsv($fo, $movie);
+	}
+
+	fclose($fo);
+
+}
+
+
+// Get first page dom.
+get_dom("https://www.imdb.com/search/title/?title_type=movie&genres=sci-fi&sort=user_rating,desc&explore=title_type,genres", "get_movies");
+
+echo json_encode($movies);
+/*
+echo '<pre>';
+print_r($movies);
+echo '</pre>';
+*/
+?>
